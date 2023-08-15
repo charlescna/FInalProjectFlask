a09d606d0b92ede3281c52e4d7350295ac73abe4
Charles <charles.anyanwu36@ed.cna.nl.ca>
2023-08-15 01:41:15 -0600

    implementing title and h1 tags on base.html and application

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('base.html', head_title= 'Cooking Recipes', page_title='Welcome to the collection of cooking recipes')
 
 @app.route('/variabletest/<name>')
 def print_variable(name):
