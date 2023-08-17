7beb88a1c186626a5ed749063cddab4dffe50745
Charles <charles.anyanwu36@ed.cna.nl.ca>
2023-08-17 00:46:30 -0600

    form validation

---
+++ templates/forms.py
@@ -0,0 +1,13 @@
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileRequired, FileField
+from wtforms.fields import StringField, TextAreaField
+from wtforms.fields.html5 import DateField, EmailField, TelField
+from wtforms.validators import DataRequired, Length
+
+
+class RecipeForm(FlaskForm):
+    recipe_name = StringField('Recipe_Name:', validators=[DataRequired()])
+    ingredients_list = TextAreaField('List of Ingredients:', validators=[DataRequired(), Length(200)])
+    preparation_instructions = TextAreaField('Preparation Instructions:', validators=[DataRequired(), Length(300)])
+    serving_instructions = TextAreaField('Serving Instructions:', validators=[DataRequired(), Length(100)])
